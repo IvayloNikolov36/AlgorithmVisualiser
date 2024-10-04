1c622abe0822418af6b49287e4a50c44e1be4cf3
Ivaylo Nikolov <ivailonikolov36@gmail.com>
2024-10-05 00:08:50 +0300

    ui fixes and refactoring

--- algorithm-visualizer/src/components/KnightsTour.jsx
+++ algorithm-visualizer/src/components/KnightsTour.jsx
@@ -1,16 +1,22 @@
 import { useEffect, useRef, useState } from "react";
 import { WhiteCell } from "../constants/board-constants";
 import { generateBoard } from "../functions/chess-board-functions";
+import { setTimeOutAfter } from "../helpers/thread-sleep";
+import { ChessSquare } from "../models/chess-square";
 import { cloneDeep, isNumber, orderBy } from "lodash";
 import { Button, ButtonGroup } from "react-bootstrap";
 
 const BoardRows = 8;
 const BoardCols = 8;
+const WaitInSeconds = 0.1;
 
 export function KnightsTour() {
 
     const [board, setBoard] = useState([]);
     const [canSelectCell, setCanSelectCell] = useState(true);
+    const [isInProgress, setIsInProgress] = useState(false);
+    const [title, setTitle] = useState('Select square to place the Knight');
+    const [showButtons, setShowButtons] = useState(false);
     const knightRow = useRef(null);
     const knightCol = useRef(null);
 
@@ -22,6 +28,9 @@ export function KnightsTour() {
         if (!canSelectCell) {
             return;
         }
+
+        setTitle('');
+        setShowButtons(true);
 
         const cell = board[row][col];
         setHasKnight(cell, true);
@@ -41,70 +50,78 @@ export function KnightsTour() {
                 setTurn(cell, null);
             });
         });
+
         setCanSelectCell(true);
         setBoard(cloneDeep(board));
     }
 
     const nextMove = () => {
-        const moves = getPossibleCellsForMove(knightRow.current, knightCol.current);
-
-        const currentTurn = getTurn(board[knightRow.current][knightCol.current]);
-
-        if (moves.length === 0 && currentTurn === (BoardRows * BoardCols)) {
+        if (knightRow.current === null) {
             return;
         }
 
-        // TODO: create cell class with X, Y and accessibleLevel
-        const evaluatedMoves = moves.map((move) => {
-            const row = move[0];
-            const col = move[1];
-            return [row, col, calculateCellAccessibleLevel(row, col)];
+        const moves = getPossibleCellsForMove({ row: knightRow.current, col: knightCol.current });
+
+        if (moves.length === 0 && getCurrentTurn() === (BoardRows * BoardCols)) {
+            return;
+        }
+
+        moves.forEach((chessSquare) => {
+            chessSquare.accessibleLevel = calculateCellAccessibleLevel(chessSquare)
         });
 
-        const destination = orderBy(evaluatedMoves, cell => cell[2], "asc")[0];
-        moveKnightTo(destination);
+        const orderedMoves = orderBy(moves, chessSquare => chessSquare.accessibleLevel, "asc");
+
+        moveKnightTo(orderedMoves[0]);
     }
 
-    const moveKnightTo = ([row, col]) => {
+    const moveKnightTo = (square) => {
         const currentCell = board[knightRow.current][knightCol.current];
         const currentTurn = getTurn(currentCell);
         setHasKnight(currentCell, false);
-        knightRow.current = row;
-        knightCol.current = col;
+        knightRow.current = square.row;
+        knightCol.current = square.col;
 
-        const newCell = board[row][col];
+        const newCell = board[square.row][square.col];
         setHasKnight(newCell, true);
         setTurn(newCell, currentTurn + 1);
 
         setBoard(cloneDeep(board));
     }
 
-    const completeTour = () => {
+    const completeTour = async () => {
+        setIsInProgress(true);
 
+        while (getCurrentTurn() < BoardRows * BoardCols) {
+            nextMove();
+            await setTimeOutAfter(WaitInSeconds);
+        }
+
+        setIsInProgress(false);
     }
 
-    const getPossibleCellsForMove = (currentRow, currentCol) => {
+    const getPossibleCellsForMove = (square) => {
         const validCells = [];
 
-        const knightMoves = getKnightMoves(currentRow, currentCol);
+        const knightMoves = getKnightMoves(square.row, square.col);
 
-        knightMoves.forEach(([row, col]) => {
-            const insideMatrix = areCoordinatesInsideBoard(row, col);
-            if (insideMatrix && !isCellVisited(row, col)) {
-                validCells.push([row, col]);
+        knightMoves.forEach((chessSquare) => {
+            const insideMatrix = areCoordinatesInsideBoard(chessSquare.row, chessSquare.col);
+            if (insideMatrix && !isCellVisited(chessSquare.row, chessSquare.col)) {
+                validCells.push(chessSquare);
             }
         });
 
         return validCells;
     }
 
-    const calculateCellAccessibleLevel = (row, col) => {
-        const moves = getPossibleCellsForMove(row, col);
+    const calculateCellAccessibleLevel = (square) => {
+        const moves = getPossibleCellsForMove(square);
 
         let count = 0;
-        moves.forEach(([cellRow, cellCol]) => {
+        moves.forEach((chessSquare) => {
 
-            if (!isCellVisited(cellRow, cellCol)) {
+            if (!isCellVisited(chessSquare.row, chessSquare.col)) {
                 count++;
             }
         });
@@ -115,14 +132,14 @@ export function KnightsTour() {
 
     const getKnightMoves = (currentRow, currentCol) => {
         return [
-            [currentRow - 2, currentCol - 1],
-            [currentRow - 1, currentCol - 2],
-            [currentRow + 1, currentCol - 2],
-            [currentRow + 2, currentCol - 1],
-            [currentRow - 2, currentCol + 1],
-            [currentRow - 1, currentCol + 2],
-            [currentRow + 1, currentCol + 2],
-            [currentRow + 2, currentCol + 1],
+            new ChessSquare(currentRow - 2, currentCol - 1),
+            new ChessSquare(currentRow - 1, currentCol - 2),
+            new ChessSquare(currentRow + 1, currentCol - 2),
+            new ChessSquare(currentRow + 2, currentCol - 1),
+            new ChessSquare(currentRow - 2, currentCol + 1),
+            new ChessSquare(currentRow - 1, currentCol + 2),
+            new ChessSquare(currentRow + 1, currentCol + 2),
+            new ChessSquare(currentRow + 2, currentCol + 1)
         ];
     }
 
@@ -142,39 +159,57 @@ export function KnightsTour() {
         return cell[2];
     }
 
+    const getCurrentTurn = () => {
+        return board[knightRow.current][knightCol.current][2];
+    }
+
     const isCellVisited = (row, col) => {
         return isNumber(board[row][col][2]);
     }
 
+    const getSquareStyle = (cellColor) => {
+        const style = 'd-flex justify-content-center align-items-center';
+        const styleDynamicPart = canSelectCell ? `chessCellSelectable ${cellColor}` : `chessCell ${cellColor}`;
+        return `${style} ${styleDynamicPart}`;
+    }
+
     return (
         <>
-            <div className="d-flex justify-content-center mt-2">
-                <ButtonGroup>
-                    <Button onClick={completeTour} variant="primary">
-                        Complete Knight's Tour
-                    </Button>
-                    <Button onClick={nextMove} variant="outline-primary">
-                        Next Move
-                    </Button>
-                    <Button onClick={clearBoard} variant="outline-primary">
-                        Clear Board
-                    </Button>
-                </ButtonGroup>
-            </div>
+            {showButtons &&
+                <div className="d-flex justify-content-center mt-2">
+                    <ButtonGroup>
+                        <Button onClick={completeTour} disabled={isInProgress || canSelectCell} variant="primary">
+                            Complete Knight's Tour
+                        </Button>
+                        <Button onClick={nextMove} disabled={isInProgress || canSelectCell} variant="outline-primary">
+                            Next Move
+                        </Button>
+                        <Button onClick={clearBoard} disabled={isInProgress || canSelectCell} variant="outline-primary">
+                            Clear Board
+                        </Button>
+                    </ButtonGroup>
+                </div>
+            }
+            {
+                title?.length > 0 &&
+                <div className="d-flex text-info justify-content-center mt-2 knightsTourTitle">
+                    <h3>{title}</h3>
+                </div>
+            }
             <div className="d-flex justify-content-center mt-3">
                 <div className="chessBorder">
                     {
                         board.map((rowArray, rowIndex) => {
                             return <div className="d-flex" key={rowIndex}>
                                 {
-                                    rowArray.map(([cellColor, hasKnight, turn], colIndex) => {
+                                    rowArray?.map(([cellColor, hasKnight, turn], colIndex) => {
                                         return <div
                                             onClick={() => clickBoardCell(rowIndex, colIndex)}
-                                            className={canSelectCell ? `chessCellSelectable ${cellColor}` : `chessCell ${cellColor}`}
+                                            className={getSquareStyle(cellColor)}
                                             key={colIndex}
                                             style={{ color: `${cellColor === WhiteCell ? 'black' : 'white'}` }}
                                         >
-                                            {hasKnight ? <span>&#9816;</span> : <span>{turn}</span>}
+                                            {hasKnight ? <span>&#9816;</span> : <span className="fs-2">{turn}</span>}
                                         </div>
                                     })
                                 }
---
+++ algorithm-visualizer/src/models/chess-square.js
@@ -0,0 +1,7 @@
+export class ChessSquare {
+    constructor(row, col, accessibleLevel) {
+        this.row = row;
+        this.col = col;
+        this.accessibleLevel = accessibleLevel;
+    }
+}
