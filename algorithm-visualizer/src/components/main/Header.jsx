62ed76ff0dec388f4df6925ae8dcf8a9d113e6ec
Ivaylo Nikolov <ivailonikolov36@gmail.com>
2024-10-19 18:44:31 +0300

    nav links rearrangement

--- algorithm-visualizer/src/components/main/Header.jsx
+++ algorithm-visualizer/src/components/main/Header.jsx
@@ -7,13 +7,13 @@ export function Header() {
     return (
         <div className="header d-flex align-items-center px-3">
             <div className="d-flex justify-content-start align-middle nav">
-                <NavLink to={routes.PathFinding}>Path Finding</NavLink>
-                <NavLink to={routes.EightQueens}>Eight Queens</NavLink>
-                <NavLink to={routes.TowerOfHanoi}>Tower Of Hanoi</NavLink>
-                <NavLink to={routes.Sorting}>Sorting Algorithms</NavLink>
-                <NavLink to={routes.KnightsTour}>Knight's Tour</NavLink>
-                <NavLink to={routes.Prims}>Prim's</NavLink>
                 <NavLink to={routes.Dijkstras}>Dijkstra's</NavLink>
+                <NavLink to={routes.Prims}>Prim's</NavLink>
+                <NavLink to={routes.Sorting}>Sorting</NavLink>
+                <NavLink to={routes.EightQueens}>Eight Queens</NavLink>
+                <NavLink to={routes.KnightsTour}>Knight's Tour</NavLink>
+                <NavLink to={routes.PathFinding}>Labyrinth Path</NavLink>
+                <NavLink to={routes.TowerOfHanoi}>Hanoi's Tower</NavLink>
             </div>
         </div>
     );
